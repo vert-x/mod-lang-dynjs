a5222656912d8eac9776ab02eb29733ae45e5bfa
Lance Ball <lball@redhat.com>
2013-04-09 13:31:35 -0400

    Add support/settings.xml and deferred test for HTTP pause/resume.

---
+++ src/test/resources/http/http_pause_resume_test.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+load('vertx.js');
+load('vertx_tests.js');
+
+var TestUtils = require('test_utils.js');
+var tu = new TestUtils();
+var port = 9090
+var server = vertx.createHttpServer();
+var client = vertx.createHttpClient().setPort(port);
+var logger = vertx.logger;
+
+function deferredTestPauseAndResume() {
+  var expectedServer = 'Request Body from Client';
+  var resultServer = '';
+  var expectedClient = 'Response Body from Server';
+  var resultClient = '';
+
+  server.requestHandler(function(request) {
+    print("PAUSING SERVER");
+    request.pause();
+    vertx.setTimer(100, function() {
+      print("RESUMING SERVER");
+      request.resume();
+      request.dataHandler(function(buffer) {
+        print("SERVER DATA RECEIVED: " + buffer);
+        resultServer += buffer;
+      });
+      request.endHandler(function() {
+        print("SERVER WRITING RESPONSE");
+        request.response.end(expectedClient);
+        print("SERVER RESPONSE ENDED");
+      });
+    });
+  });
+
+  server.listen(port, function() {
+    print("SERVER LISTENING");
+    var request = client.post('/', function(response) {
+      print("PAUSING CLIENT");
+      response.pause();
+      vertx.setTimer(100, function() {
+        response.dataHandler(function(buffer) {
+          print("CLIENT DATA RECEIVED: " + chunk);
+          resultClient += buffer;
+        });
+        response.endHandler(function() {
+          print("RESPONSE ENDED");
+          server.close();
+          vassert.assertEqual(expectedServer, resultServer);
+          vassert.assertEqual(expectedClient, resultClient);
+          vassert.testComplete();
+        });
+        print("RESUMING CLIENT");
+        response.resume();
+      });
+    });
+    request.end(expectedServer);
+  });
+}
+
+initTests(this);
